647db2d2c3d44749e45fa900d135cdab4360afab
Carlos Henrique <carlos.silva@novaandradina.org>
2021-10-07 23:56:50 -0400

    feat: add modal component

---
+++ src/components/Modal/styles.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components/native';
+import colors from '../../styles/colors';
+
+export const Container = styled.View`
+  background-color: ${colors.white};
+  padding: 10% 8%;
+  border-radius: 15px;
+  width: 335px;
+  height: 605px;
+`;
