7e2f15c42d05ba08f293a86d1e7709d32f488d89
Carlos Henrique <carlos.silva@novaandradina.org>
2021-10-08 00:00:01 -0400

    feat: add colors css

---
+++ src/screens/Welcome/styles.ts
@@ -0,0 +1,45 @@
+import styled from 'styled-components/native';
+import { Platform } from 'react-native';
+
+export const Container = styled.View`
+  flex: 1;
+  align-items: center;
+  justify-content: space-between;
+  padding: 0 30px ${Platform.OS === 'android' ? 150 : 40}px;
+`;
+
+export const Title = styled.Text`
+  font-family: "NotoSans_700Bold";
+  font-style: normal;
+  font-weight: bold;
+  font-size: 36px;
+  line-height: 49px;
+  
+  color: #FFFFFF;
+`;
+
+export const Paragraph = styled.Text`
+  font-family: "NotoSans_400Regular";
+  font-style: normal;
+  font-weight: normal;
+  font-size: 18px;
+  line-height: 25px;
+  margin-bottom: 8%
+
+  color: #6E6680;
+`;
+
+export const Actions = styled.View`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  width: 100%;
+  
+  bottom: 0;
+`;
+
+export const ContainerLogo = styled.View`
+  flex: 1;
+  align-items: center;
+  justify-content: center;
+`;
---
+++ src/styles/colors.ts
@@ -0,0 +1,7 @@
+export default {
+  pink: '#FF73F3',
+  purple: '#8142E4',
+  blue: '#6695FF',
+  gray: '#A6A1B2',
+  white: '#FFFFFF',
+};
