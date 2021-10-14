e26fc96b80c3082b6c8add78e8907261ee8f2da9
luisgaboardi <luis.ggaboardi@gmail.com>
2021-10-13 21:02:29 -0300

    Apply ESLint suggestions

--- src/screens/Success/index.tsx
+++ src/screens/Success/index.tsx
@@ -1,27 +1,27 @@
-import React from "react";
-import { Container, Image, Title, Message, Text } from "./styles";
+import React from 'react';
+import {
+  Container,
+  Image,
+  Title,
+  Message,
+  Text,
+} from './styles';
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
-import Done from "../../assets/done.svg";
-import LargePurpleButton from "../../components/Buttons/LargePurpleButton";
+import Done from '../../assets/done.svg';
+import LargePurpleButton from '../../components/Buttons/LargePurpleButton';
 
-
-function Success({ route }) {
-  const {title, message, buttonText } = route.params;
-
-  return (
-    <Container>
-      <Image>
-        <Done style={Done}/>
-      </Image>
-      <Text>
-        <Title>{title}</Title>
-        <Message>{message}</Message>
-      </Text>
-      <LargePurpleButton>{buttonText}</LargePurpleButton>
-    </Container>
-
-  );
-};
+const Success = () => (
+  <Container>
+    <Image>
+      <Done style={Done} />
+    </Image>
+    <Text>
+      <Title>{}</Title>
+      <Message>{}</Message>
+    </Text>
+    <LargePurpleButton>{}</LargePurpleButton>
+  </Container>
+);
 
 export default Success;
